2af2b1ce4a7862614f279aa4d391cdb9bcaeb56b
dachari <dachari@DIN76000182.corp.capgemini.com>
2016-09-07 18:13:41 +0530

    First Commit

---
+++ MyFirstGitApplication/src/main/java/org/cap/MyFirstGitApplication/App.java
@@ -0,0 +1,17 @@
+package org.cap.MyFirstGitApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void display(){
+    	System.out.println("SImple GIT applciation");
+    }
+}
